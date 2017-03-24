9c20e430fde2d3ed2b3a8f1d38ef7b2ad9638201
Android2017 <hilayc9@gmail.com>
2017-03-24 12:59:45 +0300

    Initial commit

---
+++ app/src/main/java/hilay/edu/colorpicker/MainActivity.java
@@ -0,0 +1,56 @@
+package hilay.edu.colorpicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.constraint_demo);
+
+
+
+
+
+
+
+/**
+ SeekBar simpleSeekBar;
+ // initiate  views
+ simpleSeekBar = (SeekBar) findViewById(R.id.seekBar);
+ // perform seek bar change listener event used for getting the progress value
+ simpleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+ int progressChangedValue = 0;
+
+ public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+ progressChangedValue = progress;
+ }
+
+ public void onStartTrackingTouch(SeekBar seekBar) {
+ // TODO Auto-generated method stub
+ }
+
+ public void onStopTrackingTouch(SeekBar seekBar) {
+ //                    Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+ //                            Toast.LENGTH_SHORT).show();
+ EditText ed1 = (EditText) findViewById(R.id.editText);
+ ed1.setText("" + progressChangedValue);
+ }
+ });
+ */
+
+
+        }
+
+
+    }
